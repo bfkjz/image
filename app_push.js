b6eb7a63c673ad90e544260fe48015ea6e91e1c3
bfkjz <69901066+bfkjz@users.noreply.github.com>
2021-10-26 14:19:06 +0800

    Create app_push.js

---
+++ app_push.js
@@ -0,0 +1,6 @@
+function cls(){
+    var mbx = document.getElementsByClassName('mbx')
+    for (var i = 0; i < mbx.length; i++){
+        mbx[i].style.cssText = 'background: #f6f6f6;margin: 5px;border-radius: 15px;padding: 15px;'
+    };
+}
