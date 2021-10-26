0079fe5ebae7af2f4f432f5006e50f6a747a87b4
bfkjz <69901066+bfkjz@users.noreply.github.com>
2021-10-26 14:19:38 +0800

    Update app_push.js

--- app_push.js
+++ app_push.js
@@ -1,5 +1,5 @@
 function cls(){
-    var mbx = document.getElementsByClassName('mbx')
+    var mbx = document.getElementsByClassName('mbx');
     for (var i = 0; i < mbx.length; i++){
         mbx[i].style.cssText = 'background: #f6f6f6;margin: 5px;border-radius: 15px;padding: 15px;'
     };
